b1559b51c3d3779cd2d73e9ab70d8655c23d610f
Yu Jung Eun <0hh.0553@gmail.com>
2022-09-13 20:08:17 +0900

    commit

---
+++ Memo/app/src/main/res/drawable/studymemo/FolderPage.java
@@ -0,0 +1,14 @@
+package com.cookandroid.studymemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FolderPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_folder_page);
+    }
+}
---
+++ Memo/app/src/main/res/drawable/studymemo/memo_page.java
@@ -0,0 +1,14 @@
+package com.cookandroid.studymemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class memo_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo_page);
+    }
+}
