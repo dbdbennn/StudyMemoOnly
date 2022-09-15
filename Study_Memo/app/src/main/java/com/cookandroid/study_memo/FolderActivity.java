186d5b1d9e73f2e9866ffab1995f277015821701
Yu Jung Eun <0hh.0553@gmail.com>
2022-09-15 20:04:09 +0900

    dialog만 못함

--- Study_Memo/app/src/main/java/com/cookandroid/study_memo/FolderActivity.java
+++ Study_Memo/app/src/main/java/com/cookandroid/study_memo/FolderActivity.java
@@ -1,8 +1,18 @@
 package com.cookandroid.study_memo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class FolderActivity extends AppCompatActivity {
 
@@ -10,5 +20,6 @@ public class FolderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
