7d8ec62cc768fd936f7c8d45d10538e7bdf28cfe
Yu Jung Eun <0hh.0553@gmail.com>
2022-10-11 20:05:45 +0900

    DB 하기 전. star 이미지 변경부터 해보자

--- Study_Memo/app/src/main/java/com/cookandroid/study_memo/FolderActivity.java
+++ Study_Memo/app/src/main/java/com/cookandroid/study_memo/FolderActivity.java
@@ -11,15 +11,39 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class FolderActivity extends AppCompatActivity {
-
+    ImageButton btnFolderPlus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnFolderPlus = findViewById(R.id.btnFolderPlus);
 
+        btnFolderPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(FolderActivity.this);
+                dlg.setTitle("폴더 추가");
+                final View dlgView = (View)View.inflate(FolderActivity.this, R.layout.folder_name_dialog, null); //dlg.xml을 java로 가져온 거임.
+                dlg.setView(dlgView);
+                dlg.setPositiveButton("생성", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), "생성됐습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), "취소됐습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dlg.show();
+            }
+        });
     }
 }
