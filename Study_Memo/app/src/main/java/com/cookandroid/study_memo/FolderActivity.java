afbf8e4ae0b50ca74cb90725040b1a94185d7a81
Yu Jung Eun <0hh.0553@gmail.com>
2022-10-25 20:02:26 +0900

    디렉토리 생성하고싶음

--- Study_Memo/app/src/main/java/com/cookandroid/study_memo/FolderActivity.java
+++ Study_Memo/app/src/main/java/com/cookandroid/study_memo/FolderActivity.java
@@ -15,19 +15,28 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class FolderActivity extends AppCompatActivity {
-    ImageButton btnFolderPlus;
+    ImageButton btnFolderPlus, btnCategoryPlus;
+    EditText category_name, folder_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnFolderPlus = findViewById(R.id.btnFolderPlus);
+        btnCategoryPlus = findViewById(R.id.btnCategoryPlus);
+        category_name = findViewById(R.id.category_name);
+        folder_name = findViewById(R.id.folder_name);
 
         btnFolderPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder dlg = new AlertDialog.Builder(FolderActivity.this);
                 dlg.setTitle("폴더 추가");
+                dlg.setIcon(R.drawable.folder_plus);
                 final View dlgView = (View)View.inflate(FolderActivity.this, R.layout.folder_name_dialog, null); //dlg.xml을 java로 가져온 거임.
                 dlg.setView(dlgView);
                 dlg.setPositiveButton("생성", new DialogInterface.OnClickListener() {
@@ -45,5 +54,35 @@ public class FolderActivity extends AppCompatActivity {
                 dlg.show();
             }
         });
+
+        btnCategoryPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(FolderActivity.this);
+                dlg.setTitle("카테고리 추가");
+                dlg.setIcon(R.drawable.category);
+                final View dlgView = (View)View.inflate(FolderActivity.this, R.layout.category_name_dialog, null); //dlg.xml을 java로 가져온 거임.
+                dlg.setView(dlgView);
+                dlg.setPositiveButton("생성", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String dirPath = "sdcard/myMemo/"+category_name;
+                        File file = new File(dirPath);
+                        if(!file.exists())
+                            file.mkdir();
+                        Toast.makeText(getApplicationContext(), "생성됐습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), "취소됐습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dlg.show();
+            }
+        });
+
+
     }
 }
